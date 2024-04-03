bc006e90b41f8e84c1316daa3efe3b4f8600dba6
Omalichanwa-1 <155693270+Omalichanwa-1@users.noreply.github.com>
2024-04-03 02:08:14 -0700

    correction

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -7,7 +7,7 @@ const Signin=() =>{
                 <div className=" mt-5  mr-3 ml-3  sm:mr-10 sm:ml-10 md:mr-20 md:ml-20"> {/*The writing side */}
                    <div className="flex justify-between">
                     <img src="/Logo.svg" alt=".." />
-                    <a href="http://localhost:5173/search" className=" flex items-center" ><span class="ml-2 text-xl">&#8592;</span> Back to campground</a>
+                    <a href="https://main--yelpcampcodewithomali.netlify.app/search" className=" flex items-center" ><span class="ml-2 text-xl">&#8592;</span> Back to campground</a>
                     </div> 
                     <h1 className=" pt-5 md:pt-20  sm:text-4xl font-bold text-2xl ">Start exploring camps from all over the world.</h1>
                     <h1 className=" pb-3 pt-3">Username</h1>
@@ -17,7 +17,7 @@ const Signin=() =>{
                     <div className=" pt-5"><Link to='/searchpage' ><button className=" bg-black text-white pl-32 pr-32  sm:pl-48 sm:pr-48 md:pl-48 md:pr-48 pt-2 pb-2">Login</button></Link></div>
                     <div className=" flex pt-2 md:pb-32 pb-3">
                         <p>Not yet a user yet?</p>
-                        <a href="http://localhost:5173/Signup" className=" text-blue-400 underline"> create an account</a>
+                        <a href="https://main--yelpcampcodewithomali.netlify.app/Signup" className=" text-blue-400 underline"> create an account</a>
                     </div>
                 </div>
                 <div className=" bg-amber-50 md:pl-28 md:pr-28 md:pt-60 sm:pt-16 sm:pb-16 sm:pr sm:pl-3 " >{/* second container*/} 
